daa7b05b48a55bce7a04760f4a74bb579efd7c08
Syedkhasim786 <syedkhasim2001@gmail.com>
2026-04-13 07:22:26 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -10,11 +10,11 @@ def agent(query, lang="english"):
     elif "predict" in q:
         key = predict_crop("black", "summer")
 
-    elif any(word in q for word in ["soil", "crop", "grow", "plant", "cultivate", "sow", "recommend"]):
-        key = get_crop_recommendation(query)
-
     elif any(word in q for word in ["season", "summer", "winter", "rainy", "monsoon", "kharif", "rabi"]):
         key = get_season_advice(query)
+
+    elif any(word in q for word in ["soil", "crop", "grow", "plant", "cultivate", "sow", "recommend"]):
+        key = get_crop_recommendation(query)
 
     elif any(word in q for word in ["fertilizer", "fertiliser", "manure", "compost", "nutrient", "urea"]):
         key = get_fertilizer_advice(query)
